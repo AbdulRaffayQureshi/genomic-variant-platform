ef155e7cae79ec79d7eb6fa58caf42f20e9c6047
Abdul Raffay Qureshi <qureshiabdulraffay@gmail.com>
2026-08-11 14:27:31 +0500

    feat: complete ETL pipeline phases 1 to 3 (Extract, Transform, Load)

---
+++ src/extract.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import logging
+from typing import List, Dict
+
+# Set up logging so we can track the pipline's progress in the terminal
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class GenomicDataExtractor:
+    """Handles autonoumous extraction of genomic data from the Ensembl REST API."""
+
+    def __init__(self):
+        self.base_url = "https://rest.ensembl.org"
+        self.headers = {"Content-Type": "application/json"}
+
+    def fetch_variants_for_gene(self, gene_name: str) -> List[Dict]:
+        """
+        Step 1: Convert a human-readable gene name (like 'BRCA1') into an Ensembl ID.
+        Step 2: Fetch known variants (mutations) for that gene.
+        """
+        logging.info(f"Looking up Ensembl ID for gene: {gene_name}")
+
+        # Step 1: Look up the Gene ID ---
+        lookup_url = f"{self.base_url}/lookup/symbol/homo_sapiens/{gene_name}?expand=1"
+        response = requests.get(lookup_url, headers=self.headers)
+
+        if response.status_code != 200:
+            logging.error(f"Failed to find gene {gene_name}. API returned {response.status_code}")
+            return []
+
+        gene_data = response.json()
+        gene_id = gene_data.get('id')
+        logging.info(f"Found Ensembl ID: {gene_id}")
+
+        # --- Step 2: Fetch Variants associated with this Gene ---
+        logging.info(f"Fetching variants for: {gene_id}...")
+        overlap_url = f"{self.base_url}/overlap/id/{gene_id}?feature=variation"
+        var_response = requests.get(overlap_url, headers=self.headers)
+
+        if var_response.status_code != 200:
+            logging.error(f"Failed to fetch variants. API returned {var_response.status_code}")
+            logging.error(f"Ensembl API Error Details: {var_response.text}")
+            return []
+
+        variants = var_response.json()
+        logging.info(f"Successfully extracted {len(variants)} variants for {gene_name}.")
+        return variants
+
+# --- Test the Extractor ---
+if __name__ == "__main__":
+    extractor = GenomicDataExtractor()
+
+    # We will test this with BRCA1 (a gene heavily associated with breast cancer)
+    hbb_variants = extractor.fetch_variants_for_gene("HBB")
+
+    # Print the first variant to see the raw data structure
+    if hbb_variants:
+        print("\n--- Sample Raw Data Extracted ---")
+        print(json.dumps(hbb_variants[0], indent=2))
---
+++ src/transform.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import logging
+from typing import List, Dict
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class GenomicDataTransformer:
+    """Handles the cleaning and structuring of raw genomic JSON data."""
+
+    def clean_variants(self, raw_variants: List[Dict]) -> pd.DataFrame:
+        """
+        Takes raw JSON data from Ensembl and flattens it into a Pandas DataFrame.
+        """
+        logging.info(f"Transforming{len(raw_variants)} raw variants into a structured table...")
+
+        # Creating an empty list to hold our cleaned rows
+        cleaned_data = []
+
+        for variant in raw_variants:
+            # We only extract the exact data points we need for our database
+            # Using .get() prevents the script from crashing if a field is missing
+            clean_row = {
+                "variant_id": variant.get("id"),
+                "chromosome": variant.get("seq_region_name"),
+                "start_position": variant.get("start"),
+                "end_position": variant.get("end"),
+                "consequence": variant.get("consequence_type"),
+                # clinical_significance is usually a list, so we join it into a single string (e.g., "pathogenic, risk_factor")
+                "clinical_significance": ", ".join(variant.get("clinical_significance", []))
+            }
+            cleaned_data.append(clean_row)
+
+        # Convert the list of cleaned Dictionaries into a Pandas DataFrame
+        df = pd.DataFrame(cleaned_data)
+
+        logging.info("Transformation complete.")
+        return df
+
+# --- Test the Transformer ---
+if __name__ == "__main__":
+    # To test this, we have to import our Phase 1 extractor!
+    from extract import GenomicDataExtractor
+
+    extractor = GenomicDataExtractor()
+    transformer = GenomicDataTransformer()
+
+    # 1. Extract the raw data (Phase 1)
+    raw_data = extractor .fetch_variants_for_gene("HBB")
+
+    # 2. Transform the data (Phase 2)
+    clean_df = transformer.clean_variants(raw_data)
+
+    # 3. Print the first 5 rows of the cleaned DataFrame to verify
+    print("\n--- Sample Cleaned Data (Top 5 Rows)---")
+    print(clean_df.head())
+
+
